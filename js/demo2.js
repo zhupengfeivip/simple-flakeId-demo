a33c47ddd27c139267b85a2636f83de26f38e670
zhupengfei <zhupengfeivip@163.com>
2021-10-26 16:08:44 +0800

    f

---
+++ js/demo2.js
@@ -0,0 +1,7 @@
+var genid = require('simple-flakeid').genid
+
+let gen1 = new genid({ WorkerId: 1 })
+for (let i = 0; i < 10; i++) {
+    let id1 = gen1.NextBigId()
+    console.log(`${i} ID:${id1} ${typeof id1} 长度：${id1.toString().length}`)
+}
